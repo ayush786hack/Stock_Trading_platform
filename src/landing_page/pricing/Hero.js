2ba5d040a7f693a2193af86dd4e19eb750d16dc7
Ayush Tiwari <ayushtiwari92205@gmail.com>
2026-06-08 21:31:58 +0530

    pricing page

--- src/landing_page/pricing/Hero.js
+++ src/landing_page/pricing/Hero.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-function Hero() {
+ function Hero() {
     return ( 
-        <h1>Hero</h1>
+          <div className="container">
+     
+       
+
+      <div className="row p-5 mt-5 border-bottom text-center">     
+        <h1 >Pricing</h1>
+        <h3 className="text-muted mt-3 fs-4">Free equity investments and flat ₹20  traday and F&O trades.
+        </h3>
+         </div>
+        <div className="row p-5 mt-5">
+          <div className="col-4"
+        </div>
+     
+    </div>
      );
 }
 
---
+++ src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hero from './Hero';
+import Brokerage from './Brokerage';
+import OpenAccount from '../OpenAccount';
+
+
+export default function ProductPage(){
+    return(
+        <>
+        <Hero/>
+        <OpenAccount/>
+        <Brokerage/>
+        </>
+    )
+}
